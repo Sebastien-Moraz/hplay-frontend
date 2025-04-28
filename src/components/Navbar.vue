62eee4f41f0edc6177fd86b924c404f043455b4b
Sebastien-Moraz <sebastien@moraz.net>
2025-04-28 09:58:14 +0200

    feat: setup default routing with navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,114 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const isMenuOpen = ref(false)
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+}
+</script>
+
+<template>
+  <nav class="navbar">
+    <!-- Logo -->
+    <div class="logo">
+      <RouterLink to="/">
+        <img src="@/assets/logo.png" alt="Logo" />
+      </RouterLink>
+    </div>
+
+    <!-- Burger Menu -->
+    <img src="@/assets/burgerMenu.svg" alt="Logo" class="burger-menu" @click="toggleMenu" />
+
+    <!-- Menu Links -->
+    <ul class="menu" :class="{ open: isMenuOpen }">
+      <li><RouterLink to="/">Home</RouterLink></li>
+      <li><RouterLink to="/store">Store</RouterLink></li>
+      <li><RouterLink to="/cart">Cart</RouterLink></li>
+      <li><RouterLink to="/about">About</RouterLink></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #1d1d1d;
+}
+
+.logo img {
+  height: 40px;
+}
+
+.burger-menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+}
+
+.burger-menu span {
+  display: block;
+  height: 3px;
+  background-color: #333;
+  border-radius: 2px;
+}
+
+.menu {
+  display: none;
+  flex-direction: column;
+  position: absolute;
+  top: 60px;
+  right: 1rem;
+  background-color: black;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 1rem;
+}
+
+.menu.open {
+  display: flex;
+}
+
+.menu li {
+  margin: 0.5rem 0;
+}
+
+.menu a {
+  text-decoration: none;
+  font-size: 1.4rem;
+  color: white;
+  font-weight: bold;
+}
+
+.menu a.router-link-exact-active {
+  color: red;
+}
+
+@media (min-width: 768px) {
+  .burger-menu {
+    display: none;
+    
+  }
+
+  .menu {
+    display: flex;
+    position: static;
+    flex-direction: row;
+    background: none;
+    border: none;
+    padding: 0;
+    
+  }
+
+  .menu li {
+    margin: 0 1rem;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import StoreView from '../views/StoreView.vue'
+import CartView from '../views/CartView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +12,21 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/store',
+      name: 'store',
+      component: StoreView,
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+        component: CartView,
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      redirect: () => {
+        window.location.href = 'https://hstudio.ch';
+      },
     },
   ],
 })
