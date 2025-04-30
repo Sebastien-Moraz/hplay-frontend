ff4566fe61d503462a4b74911b4a09f1076f4e59
Sebastien-Moraz <sebastien@moraz.net>
2025-04-30 15:34:54 +0200

    chore: add comment in MediaView

--- src/views/MediaView.vue
+++ src/views/MediaView.vue
@@ -5,20 +5,32 @@ import Media from "@/libs/Media.js";
 import {useRoute} from "vue-router";
 let storeId =  useRoute().params.id;
 
-const media = new Media(
-		storeId,
-		"Video demo",
-		"Ceci est la description de la vidéo de demonstration",
-		10.95,
-		"https://mfc.koppa.pro/img/hero/hero-1.jpg",
-		[
-			"2025",
-			"test",
-			"test différent",
-			"2022",
-			"2023",
-		]
-);
+const media = ref(null);
+
+// Get the media data from the API
+const loadMedia = () => {
+	//TODO: remove this and replace with API call
+	//TODO: check if media is already bought by the user
+	return new Media(
+			storeId,
+			"Video demo",
+			"Ceci est la description de la vidéo de demonstration",
+			10.95,
+			"https://mfc.koppa.pro/img/hero/hero-1.jpg",
+			[
+				"2025",
+				"test",
+				"test différent",
+				"2022",
+				"2023",
+			]
+	);
+}
+
+// Load media when the component is mounted
+onMounted(() => {
+	media.value = loadMedia();
+});
 </script>
 
 <template>
