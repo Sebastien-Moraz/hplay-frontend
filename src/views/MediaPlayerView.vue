6910020625a9274c10441cc3cbc285f57d346a7f
Sebastien-Moraz <sebastien@moraz.net>
2025-04-30 15:34:43 +0200

    chore: add comment in MediaPlayerView

--- src/views/MediaPlayerView.vue
+++ src/views/MediaPlayerView.vue
@@ -2,27 +2,54 @@
 import MediaList from "@/components/MediaList.vue";
 import Media from "@/libs/Media.js";
 import MediaPlayer from "@/components/MediaPlayer.vue";
+import { useRoute } from "vue-router";
+import {onMounted, ref} from "vue";
 
-let media = new Media(
-		1,
-		"Video demo",
-		"Ceci est la description de la vidéo de demonstration",
-		10.95,
-		"https://mfc.koppa.pro/img/hero/hero-1.jpg",
-		["2025", "test", "demo"]
-);
+let storeId = useRoute().params.id;
+let media = ref(null);
+let recommendedMedias = ref([]);
+let mediaToken = ref(null);
 
-let mediaToken = "token=st=1746000032~exp=1756479480~acl=/_hls_/1jijk03u2im16/1jhvl2uqfrkau/*~hmac=ea9c08764c3c293a8ff549832495579b1437efcf33829d7ad48a8d78a21d90f9";
-
-let recommendedMedias = [];
-for (let i = 0; i < 2; i++) {
-	recommendedMedias.push(media);
+// Get the media data from the API
+const loadMedia = () => {
+	//TODO: remove this and replace with API call
+	return new Media(
+			storeId,
+			"Video demo",
+			"Ceci est la description de la vidéo de demonstration",
+			10.95,
+			"https://mfc.koppa.pro/img/hero/hero-1.jpg",
+			["2025", "test", "demo"]
+	);
 }
+
+// Get the recommended media data by tags from the API
+const loadRecommendedMedias = () => {
+	//TODO: remove this and replace with API call
+	let recommendedMedias = [];
+	for (let i = 0; i < 2; i++) {
+		recommendedMedias.push(media.value);
+	}
+	return recommendedMedias;
+}
+
+// Get the media token from the API
+const loadMediaToken = () => {
+	//TODO: remove this and replace with API call
+	return "token=st=1746019450~exp=1754060280~acl=/_hls_/1jijk03u2im16/1jhvl2uqfrkau/*~hmac=5fb791fd30855b93d8a553cf332ea33ccc2f584022dbc9f0f9308fcada96da09";
+}
+
+// Load media, recommended media and token when the component is mounted
+onMounted(() => {
+	media.value = loadMedia();
+	recommendedMedias.value = loadRecommendedMedias();
+	mediaToken.value = loadMediaToken();
+});
 </script>
 
 <template>
 	<div class="media-view">
-		<MediaPlayer :media="media" :token="mediaToken" />
+		<MediaPlayer v-if="media" :media="media" :token="mediaToken" />
 		<div class="recommendations">
 			<h2 class="recommendations-title">Recommandations</h2>
 			<MediaList :medias="recommendedMedias" />
