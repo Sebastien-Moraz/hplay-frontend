0b04dc219283714dc97223ba1bc5180b85313b7c
Sebastien-Moraz <sebastien@moraz.net>
2025-04-30 08:28:09 +0200

    feat: add a return-to-top behavior when using the navigation

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,13 @@ import MediaView from '../views/MediaView.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
+	scrollBehavior(to, from, savedPosition) {
+		if (savedPosition) {
+			return savedPosition
+		} else {
+			return {top: 0}
+		}
+	},
 	routes: [
 		{
 			path: '/',
