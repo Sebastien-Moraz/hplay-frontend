f955843c861c47cc32e867850ba8476d04edc8ff
Sebastien-Moraz <sebastien@moraz.net>
2025-05-08 16:19:57 +0200

    refactor: remove shareId in lib Media

--- src/libs/Media.js
+++ src/libs/Media.js
@@ -1,10 +1,9 @@
 export default class Media {
-	constructor(id, name, description, price, shareId, preview, tags) {
+	constructor(id, name, description, price, preview, tags) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
-		this.shareId = shareId;
 		this.preview = preview;
 		this.tags = tags;
 	}
